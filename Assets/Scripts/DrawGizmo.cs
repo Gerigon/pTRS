fc8eb8e2cc66e90cfa660ce91c048e6583563f9c
CptFrameLag <itschris_1993@hotmail.com>
2015-08-28 02:26:25 +0200

    small start

---
+++ Assets/Scripts/DrawGizmo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawGizmo : MonoBehaviour {
+
+	Camera cam;
+	
+	void Start() {
+		cam = GetComponent<Camera>();
+	}
+	
+	void Update() {
+		Ray ray = cam.ViewportPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+		if (Physics.Raycast(ray, out hit))
+			print("I'm looking at " + hit.transform.name);
+		else
+			print("I'm looking at nothing!");
+	}
+}
---
+++ Assets/Scripts/Pathing.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathing : MonoBehaviour {
+
+	public float moveSpeed = 5;
+	public Transform marker;
+	public Camera cam;
+	public Vector3 movePoint;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetInput();
+		MoveToPos();
+	}
+
+	void GetInput() {
+		if(Input.GetMouseButtonDown(1)) {
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit) && hit.transform.tag == "ground") {
+				Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
+				movePoint = hit.point;
+				Instantiate(marker,movePoint,Quaternion.identity);
+			}
+		}
+	}
+
+	void MoveToPos() {
+		//transform.LookAt(movePoint);
+		Ray myRay = new Ray(transform.position, movePoint-transform.position);
+		
+		RaycastHit myHit;
+		if (Physics.Raycast (myRay, out myHit,5)) {
+			if(myHit.transform != transform)
+				Debug.DrawRay(transform.position, myRay.direction * 20, Color.yellow);
+		}
+		//transform.position = Vector3.MoveTowards(transform.position,movePoint,Time.deltaTime *moveSpeed);
+	}
+}
